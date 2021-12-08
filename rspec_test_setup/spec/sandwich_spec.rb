c3a1dd9f7a02d4390b8d442f109edc855d363c83
George <gchriscarter@gmail.com>
2021-12-08 12:32:22 -0500

    refactor logic in sandwich_spec.rb to reduce repetition using a let block

--- rspec_test_setup/spec/sandwich_spec.rb
+++ rspec_test_setup/spec/sandwich_spec.rb
@@ -2,16 +2,16 @@ require_relative "../sandwich.rb"
 
 describe Sandwich do
   context "when the sandwich should be vegan" do
+    let(:sandwich) { Sandwich.new(false, false, ["lettuce", "tomato", "mustard"]) }
+    # Same as
+    # let(:sandwich) do Sandwich.new(false, false, ["lettuce", "tomato", "mustard"]) end
     it "should not have meat" do
-      sandwich = Sandwich.new(false, false, ["lettuce", "tomato", "mustard"])
       expect(sandwich.meat).to eq(false)
     end
     it "should not have cheese" do
-      sandwich = Sandwich.new(false, false, ["lettuce", "tomato", "mustard"])
       expect(sandwich.cheese).to eq(false)
     end
     it "should not have mayo" do
-      sandwich = Sandwich.new(false, false, ["lettuce", "tomato", "mustard"])
       expect(sandwich.condiments).to_not include("mayo")
     end
   end
