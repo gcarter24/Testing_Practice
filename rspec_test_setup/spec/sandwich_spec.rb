c09119ed367b7679a420e4bba633bc7f91ad57b1
George <gchriscarter@gmail.com>
2021-12-08 12:44:47 -0500

    add logic to store new sandwich in a variable to show potential use in a more complicated app

--- rspec_test_setup/spec/sandwich_spec.rb
+++ rspec_test_setup/spec/sandwich_spec.rb
@@ -2,17 +2,18 @@ require_relative "../sandwich.rb"
 
 describe Sandwich do
   context "when the sandwich should be vegan" do
-    let(:sandwich) { Sandwich.new(false, false, ["lettuce", "tomato", "mustard"]) }
+    # let(:sandwich) { Sandwich.new(false, false, ["lettuce", "tomato", "mustard"]) }
+
     # Same as
     # let(:sandwich) do Sandwich.new(false, false, ["lettuce", "tomato", "mustard"]) end
     it "should not have meat" do
-      expect(sandwich.meat).to eq(false)
+      expect(vegan_sandwich.meat).to eq(false)
     end
     it "should not have cheese" do
-      expect(sandwich.cheese).to eq(false)
+      expect(vegan_sandwich.cheese).to eq(false)
     end
     it "should not have mayo" do
-      expect(sandwich.condiments).to_not include("mayo")
+      expect(vegan_sandwich.condiments).to_not include("mayo")
     end
   end
   context "when the sandwich shouldn't be vegan" do
@@ -28,4 +29,9 @@ describe Sandwich do
       expect(@sandwich.condiments).to include("mayo")
     end
   end
+end
+
+# **Storing in a method** Not as widely used for simplier things but may be useful in a large app
+def vegan_sandwich
+  Sandwich.new(false, false, ["lettuce", "tomato", "mustard"])
 end
