464f363ac74114782f87c7ceb4f3a7cb4d71aa66
George <gchriscarter@gmail.com>
2021-12-08 12:22:38 -0500

    add context to sandwich_spec.rb before adding logic/methods to sandwich class

--- rspec_test_setup/spec/sandwich_spec.rb
+++ rspec_test_setup/spec/sandwich_spec.rb
@@ -1,4 +1,18 @@
 require_relative "../sandwich.rb"
 
 describe Sandwich do
+  context "when the sandwich should be vegan" do
+    it "should not have meat" do
+      sandwich = Sandwich.new(false, false, ["lettuce", "tomato", "mustard"])
+      expect(sandwich.meat).to eq(false)
+    end
+    it "should not have cheese" do
+      sandwich = Sandwich.new(false, false, ["lettuce", "tomato", "mustard"])
+      expect(sandwich.cheese).to eq(false)
+    end
+    it "should not have mayo" do
+      sandwich = Sandwich.new(false, false, ["lettuce", "tomato", "mustard"])
+      expect(sandwich.condiments).to_not include("mayo")
+    end
+  end
 end
